fd5177260a378f484c74df9d81c4b5a74cfd3b76
viktorferenczi <ferencziviktor@icloud.com>
2026-02-21 15:20:24 +0100

    route comments

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,6 +1,10 @@
-export const ROUTE = { 
-    BOOTSTRAP: "bootstrap",
-    
+export const ROUTE = {
+  // APP
+  BOOTSTRAP: "bootstrap",
+  // AUTH
+  // PRODUCT(S)
+  // VOICE 
+  // SETTINGS
 } as const;
 
 export default ROUTE;
